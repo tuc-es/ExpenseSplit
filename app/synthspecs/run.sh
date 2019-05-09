d99dc960fed6164072e8f6f24029749fd9407531
Ruediger Ehlers <rehlers@uni-bremen.de>
2019-05-09 14:02:54 +0200

    Initial commit

---
+++ app/synthspecs/run.sh
@@ -0,0 +1,2 @@
+../../guisynth/tools/Orchestrator/orchestratorExplicit.py ../src/main/res/layout/content_main.xml mainActivitySpec.txt ../src/main/java/de/safeml/expensesplit/MainActivity.java --gameOutputFile game.txt
+
